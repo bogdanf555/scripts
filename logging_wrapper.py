42b7b36c5891da6dcde8f7889bdf0798f91bef12
Bogdan Fuia <bogdan.fuia@gmail.com>
2022-02-19 23:36:33 +0200

    updated log wrapper

--- logging_wrapper.py
+++ logging_wrapper.py
@@ -9,10 +9,15 @@ CRITICAL = 50
 WRITE = "w"
 APPEND = "a"
 
-DEFAULT_FORMAT = "[%(asctime)s] [%(levelname)8s] [ %(name)20s ] : %(message)s (%(funcName)s:%(lineno)s)"
+DEFAULT_FORMAT = (
+    "[%(asctime)s] [%(levelname)8s] : %(message)s (%(name)s:%(funcName)s:%(lineno)s)"
+)
+DEFAULT_FILENAME = "info.log"
 
 
-def init_logger(logger_name, filepath, level, format=DEFAULT_FORMAT):
+def init_logger(
+    logger_name, filepath=DEFAULT_FILENAME, level=DEBUG, format=DEFAULT_FORMAT
+):
 
     # create a logger instance
     logger = logging.getLogger(logger_name)
